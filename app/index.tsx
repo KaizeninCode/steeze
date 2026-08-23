be99a94325bc189cbd1d41cd8ce9396d58cbb0fb
Phil <philwekullo@gmail.com>
2026-08-23 12:27:12 +0300

    add Lottie anomation to home screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,17 +2,22 @@ import { useRouter } from "expo-router";
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import LottieView from 'lottie-react-native';
 
 const Home = () => {
   const router = useRouter();
   return (
     <SafeAreaView className="flex-1 bg-light dark:bg-dark">
-      <View className="flex-1 border-white my-16 mx-8 rounded-lg shadow-sm shadow-dark dark:shadow-light"></View>
+      <View className="flex-1 border-white my-16 mx-8 rounded-lg shadow-sm shadow-dark dark:shadow-light"> 
+      </View> 
+        <LottieView source={require('@/assets/snake.json')} autoPlay loop />
       <Pressable
         onPress={() => router.navigate("/create-join")}
         className="dark:bg-light bg-dark px-3 py-2 rounded-xl w-3/5 mx-auto mb-16"
       >
-        <Text className="text-center text-2xl font-alfa dark:text-dark text-light">Get Started</Text>
+        <Text className="text-center text-2xl font-alfa dark:text-dark text-light">
+          Get Started
+        </Text>
       </Pressable>
     </SafeAreaView>
   );
